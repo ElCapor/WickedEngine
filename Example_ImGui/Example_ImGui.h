7dd967bb4b3fa2c1c12aa3070a2ab0eeb055afc1
ElCapor <104172250+ElCapor@users.noreply.github.com>
2023-10-30 20:32:47 +0100

    TextEditor added - can run scripts

--- Example_ImGui/Example_ImGui.h
+++ Example_ImGui/Example_ImGui.h
@@ -1,9 +1,11 @@
 #pragma once
 #include "WickedEngine.h"
-
+#define IMGUI_EDITOR_NO_BOOST
+#include "ImGui/TextEditor.h"
 
 class Example_ImGuiRenderer : public wi::RenderPath3D
 {
+	TextEditor* editor = nullptr;
 	wi::gui::Label label;
 public:
 	void Load() override;
