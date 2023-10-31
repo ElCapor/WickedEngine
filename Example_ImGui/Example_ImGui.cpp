3db05af667d84ec3d1f5832d01bd76bc56f648f4
ElCapor <104172250+ElCapor@users.noreply.github.com>
2023-10-31 14:49:41 +0100

    funny stuff happening

--- Example_ImGui/Example_ImGui.cpp
+++ Example_ImGui/Example_ImGui.cpp
@@ -407,6 +407,223 @@ bool RunScriptImpl(const char* script, LuaErrorStruct& errorStruct)
  }
 
 bool started = false;
+bool showCodeEditor = false;
+bool showScriptingApiDoc = false;
+#include "imgui_markdown.h"
+static ImFont* H1 = NULL;
+static ImFont* H2 = NULL;
+static ImFont* H3 = NULL;
+
+static ImGui::MarkdownConfig mdConfig;
+
+inline ImGui::MarkdownImageData ImageCallback(ImGui::MarkdownLinkCallbackData data_)
+{
+	// In your application you would load an image based on data_ input. Here we just use the imgui font texture.
+	ImTextureID image = ImGui::GetIO().Fonts->TexID;
+	// > C++14 can use ImGui::MarkdownImageData imageData{ true, false, image, ImVec2( 40.0f, 20.0f ) };
+	ImGui::MarkdownImageData imageData;
+	imageData.isValid = true;
+	imageData.useLinkCallback = false;
+	imageData.user_texture_id = image;
+	imageData.size = ImVec2(40.0f, 20.0f);
+
+	// For image resize when available size.x > image width, add
+	ImVec2 const contentSize = ImGui::GetContentRegionAvail();
+	if (imageData.size.x > contentSize.x)
+	{
+		float const ratio = imageData.size.y / imageData.size.x;
+		imageData.size.x = contentSize.x;
+		imageData.size.y = contentSize.x * ratio;
+	}
+
+	return imageData;
+}
+
+void LoadFonts(float fontSize_ = 12.0f)
+{
+	ImGuiIO& io = ImGui::GetIO();
+	io.Fonts->Clear();
+	// Base font
+	io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Arial.ttf", fontSize_);
+	// Bold headings H2 and H3
+	H2 = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Verdana.ttf", fontSize_);
+	H3 = mdConfig.headingFormats[1].font;
+	// bold heading H1
+	float fontSizeH1 = fontSize_ * 1.1f;
+	H1 = io.Fonts->AddFontFromFileTTF("C:\\Windows\\Fonts\\Verdana.ttf", fontSizeH1);
+}
+
+void ExampleMarkdownFormatCallback(const ImGui::MarkdownFormatInfo& markdownFormatInfo_, bool start_)
+{
+	// Call the default first so any settings can be overwritten by our implementation.
+	// Alternatively could be called or not called in a switch statement on a case by case basis.
+	// See defaultMarkdownFormatCallback definition for furhter examples of how to use it.
+	ImGui::defaultMarkdownFormatCallback(markdownFormatInfo_, start_);
+
+	switch (markdownFormatInfo_.type)
+	{
+		// example: change the colour of heading level 2
+	case ImGui::MarkdownFormatType::HEADING:
+	{
+		if (markdownFormatInfo_.level == 2)
+		{
+			if (start_)
+			{
+				ImGui::PushStyleColor(ImGuiCol_Text, ImGui::GetStyle().Colors[ImGuiCol_TextDisabled]);
+			}
+			else
+			{
+				ImGui::PopStyleColor();
+			}
+		}
+		break;
+	}
+	default:
+	{
+		break;
+	}
+	}
+}
+
+void LinkCallback(const ImGui::MarkdownLinkCallbackData& data_) {
+	std::string url(data_.link, data_.linkLength);
+	std::cout << url << std::endl;
+	// Check if the link is a local reference (e.g., anchor within the document)
+	if (url[0] == '#') {
+		// Handle local navigation here, e.g., scroll to the specified section or anchor
+		// You would need to implement your own logic for navigating to the local content
+		// based on the anchor provided in the URL.
+		std::cout << "Local Reference" << std::endl;
+
+		// Example: Scroll to a section with the specified anchor
+		if (strcmp(url.c_str(), "#section1") == 0) {
+			// Code to scroll to the "section1" in your ImGui window
+			// Implement your logic to scroll to the desired location.
+		}
+	}
+	// Handle external links (e.g., open in a web browser) if needed
+	else {
+		// ShellExecuteA( NULL, "open", url.c_str(), NULL, NULL, SW_SHOWNORMAL );
+	}
+}
+
+
+void Markdown(const std::string& markdown_)
+{
+	// You can make your own Markdown function with your prefered string container and markdown config.
+	// > C++14 can use ImGui::MarkdownConfig mdConfig{ LinkCallback, NULL, ImageCallback, ICON_FA_LINK, { { H1, true }, { H2, true }, { H3, false } }, NULL };
+	mdConfig.linkCallback = LinkCallback;
+	mdConfig.tooltipCallback = NULL;
+	mdConfig.imageCallback = ImageCallback;
+	mdConfig.linkIcon = NULL;
+	mdConfig.headingFormats[0] = { H1, true };
+	mdConfig.headingFormats[1] = { H2, true };
+	mdConfig.headingFormats[2] = { H3, false };
+	mdConfig.userData = NULL;
+	mdConfig.formatCallback = ExampleMarkdownFormatCallback;
+	ImGui::Markdown(markdown_.c_str(), markdown_.length(), mdConfig);
+}
+
+#include "ImGui/imgui_md.h"
+
+
+//Fonts and images (ImTextureID) must be loaded in other place
+//see https://github.com/ocornut/imgui/blob/master/docs/FONTS.md
+ImFont* g_font_regular = nullptr;
+ImFont* g_font_bold = nullptr;
+ImFont* g_font_bold_large = nullptr;
+ImTextureID g_texture1 = nullptr;
+
+struct my_markdown : public imgui_md
+{
+public:
+
+	void initFont()
+	{
+		ImGuiIO& io = ImGui::GetIO();
+		g_font_regular = ImGui::GetDefaultFont();
+		g_font_bold = ImGui::GetDefaultFont();
+		g_font_bold_large = ImGui::GetDefaultFont();
+	}
+	ImFont* get_font() const override
+	{
+		if (m_is_table_header) {
+			return g_font_bold;
+		}
+
+		switch (m_hlevel)
+		{
+		case 0:
+			return m_is_strong ? g_font_bold : g_font_regular;
+		case 1:
+			return g_font_bold_large;
+		default:
+			return g_font_bold;
+		}
+	};
+
+	void open_url() const override
+	{
+		//platform dependent code
+		std::cout << "Open url lmao " << m_href << std::endl;
+	}
+
+	bool get_image(image_info& nfo) const override
+	{
+		//use m_href to identify images
+		nfo.texture_id = g_texture1;
+		nfo.size = { 40,20 };
+		nfo.uv0 = { 0,0 };
+		nfo.uv1 = { 1,1 };
+		nfo.col_tint = { 1,1,1,1 };
+		nfo.col_border = { 0,0,0,0 };
+		return true;
+	}
+
+	void html_div(const std::string& dclass, bool e) override
+	{
+		if (dclass == "red") {
+			if (e) {
+				m_table_border = false;
+				ImGui::PushStyleColor(ImGuiCol_Text, IM_COL32(255, 0, 0, 255));
+			}
+			else {
+				ImGui::PopStyleColor();
+				m_table_border = true;
+			}
+		}
+	}
+};
+
+//call this function to render your markdown
+void markdown(const std::string& aString)
+{
+	static my_markdown s_printer;
+	s_printer.initFont();
+	s_printer.print(aString);
+}
+
+
+int FindAllEntitiesLua(lua_State* L)
+{
+	wi::unordered_set<wi::ecs::Entity> listOfLoadedEntities;
+	GetScene().FindAllEntities(listOfLoadedEntities);
+
+	luaL_newmetatable(L, "test");
+	int metatable = lua_gettop(L);
+
+	for (auto entt : listOfLoadedEntities)
+	{
+		NameComponent* name = GetScene().names.GetComponent(entt);
+
+		lua_pushstring(L, name ? name->name.c_str() : "Unknown");
+		wi::lua::SSetLongLong(L, entt);
+		lua_settable(L, metatable);
+	}
+	return 1;
+	
+
+}
 void Example_ImGuiRenderer::Update(float dt)
 {
 	// Start the Dear ImGui frame
@@ -432,88 +649,134 @@ void Example_ImGuiRenderer::Update(float dt)
 		editor->SetPalette(TextEditor::GetMarianaPalette());
 		started = true;
 	}
+
 	
-	std::pair<int, int> cpos;
-	editor->GetCursorPosition(cpos.first, cpos.second);
-	ImGui::Begin("Code Editor", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
-	ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
-	if (ImGui::BeginMenuBar())
+	if (ImGui::Begin("Funny Window"))
 	{
-		if (ImGui::BeginMenu("File"))
+		ImGui::Checkbox("Code editor", &showCodeEditor);
+		
+		wi::unordered_set<wi::ecs::Entity> listOfLoadedEntities;
+		GetScene().FindAllEntities(listOfLoadedEntities);
+		if (ImGui::TreeNode("Scene"))
 		{
-			if (ImGui::MenuItem("Save"))
+			for (wi::ecs::Entity entt : listOfLoadedEntities)
 			{
-				auto textToSave = editor->GetText();
-				/// save text....
-			}
-
-			if (ImGui::MenuItem("Execute"))
-			{
-				LuaErrorStruct errorStruct;
-				RunScriptImpl(editor->GetText().c_str(), errorStruct);
-
-				std::cout << errorStruct.line << " " << errorStruct.errorMsg;
-				std::map<int, std::string> errors;
-				errors[errorStruct.line] = errorStruct.errorMsg;
-				editor->SetErrorMarkers(errors);
-
-			}
+				NameComponent* name = GetScene().names.GetComponent(entt);
 				
-			ImGui::EndMenu();
+				if (ImGui::TreeNode(name ? name->name.c_str() : "Unnamed"))
+				{
+					ImGui::SameLine();
+					if (ImGui::SmallButton("Delete"))
+					{
+						GetScene().Entity_Remove(entt);
+					}
+					ImGui::TreePop();
+				}
+				
+
+			}
+			ImGui::TreePop();
 		}
-		if (ImGui::BeginMenu("Edit"))
-		{
-			bool ro = editor->IsReadOnlyEnabled();
-			if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
-				editor->SetReadOnlyEnabled(ro);
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor->CanUndo()))
-				editor->Undo();
-			if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor->CanRedo()))
-				editor->Redo();
-
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor->AnyCursorHasSelection()))
-				editor->Copy();
-			if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor->AnyCursorHasSelection()))
-				editor->Cut();
-			if (ImGui::MenuItem("Undo", "Del", nullptr, !ro && editor->AnyCursorHasSelection()))
-				editor->Undo();
-			if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
-				editor->Paste();
-
-			ImGui::Separator();
-
-			if (ImGui::MenuItem("Select all", nullptr, nullptr))
-				editor->SelectAll();
-
-			ImGui::EndMenu();
-		}
-
-		if (ImGui::BeginMenu("View"))
-		{
-			if (ImGui::MenuItem("Dark palette"))
-				editor->SetPalette(TextEditor::GetDarkPalette());
-			if (ImGui::MenuItem("Light palette"))
-				editor->SetPalette(TextEditor::GetLightPalette());
-			if (ImGui::MenuItem("Retro blue palette"))
-				editor->SetPalette(TextEditor::GetRetroBluePalette());
-			if (ImGui::MenuItem("Mariana palette"))
-				editor->SetPalette(TextEditor::GetMarianaPalette());
-			ImGui::EndMenu();
-		}
-		ImGui::EndMenuBar();
+		std::vector<uint8_t> bytes;
+		wi::helper::FileRead("../Content/Documentation/ScriptingAPI-Documentation.md", bytes);
+		std::string doc = std::string(bytes.begin(), bytes.end());
+		Markdown(doc); //
+		//markdown(doc);
+		ImGui::End();
 	}
+	if (showCodeEditor)
+	{
+		std::pair<int, int> cpos;
+		editor->GetCursorPosition(cpos.first, cpos.second);
+		ImGui::Begin("Code Editor", nullptr, ImGuiWindowFlags_HorizontalScrollbar | ImGuiWindowFlags_MenuBar);
+		ImGui::SetWindowSize(ImVec2(800, 600), ImGuiCond_FirstUseEver);
+		if (ImGui::BeginMenuBar())
+		{
+			if (ImGui::BeginMenu("File"))
+			{
+				if (ImGui::MenuItem("Save"))
+				{
+					auto textToSave = editor->GetText();
+					/// save text....
+				}
 
-	ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.first + 1, cpos.second + 1, editor->GetLineCount(),
-		editor->IsOverwriteEnabled() ? "Ovr" : "Ins",
-		editor->CanUndo() ? "*" : " ",
-		editor->GetLanguageDefinition().mName.c_str(), "Temp.lua");
-	editor->Render("Lua Editor");
-	ImGui::Dummy(ImVec2(20, 20));
-	ImGui::End();
+				if (ImGui::MenuItem("Execute"))
+				{
+					LuaErrorStruct errorStruct;
+					RunScriptImpl(editor->GetText().c_str(), errorStruct);
+
+					std::cout << errorStruct.line << " " << errorStruct.errorMsg;
+					std::map<int, std::string> errors;
+					errors[errorStruct.line] = errorStruct.errorMsg;
+					editor->SetErrorMarkers(errors);
+
+				}
+
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Edit"))
+			{
+				bool ro = editor->IsReadOnlyEnabled();
+				if (ImGui::MenuItem("Read-only mode", nullptr, &ro))
+					editor->SetReadOnlyEnabled(ro);
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Undo", "ALT-Backspace", nullptr, !ro && editor->CanUndo()))
+					editor->Undo();
+				if (ImGui::MenuItem("Redo", "Ctrl-Y", nullptr, !ro && editor->CanRedo()))
+					editor->Redo();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Copy", "Ctrl-C", nullptr, editor->AnyCursorHasSelection()))
+					editor->Copy();
+				if (ImGui::MenuItem("Cut", "Ctrl-X", nullptr, !ro && editor->AnyCursorHasSelection()))
+					editor->Cut();
+				if (ImGui::MenuItem("Undo", "Del", nullptr, !ro && editor->AnyCursorHasSelection()))
+					editor->Undo();
+				if (ImGui::MenuItem("Paste", "Ctrl-V", nullptr, !ro && ImGui::GetClipboardText() != nullptr))
+					editor->Paste();
+
+				ImGui::Separator();
+
+				if (ImGui::MenuItem("Select all", nullptr, nullptr))
+					editor->SelectAll();
+
+				ImGui::EndMenu();
+			}
+
+			if (ImGui::BeginMenu("View"))
+			{
+				if (ImGui::MenuItem("Dark palette"))
+					editor->SetPalette(TextEditor::GetDarkPalette());
+				if (ImGui::MenuItem("Light palette"))
+					editor->SetPalette(TextEditor::GetLightPalette());
+				if (ImGui::MenuItem("Retro blue palette"))
+					editor->SetPalette(TextEditor::GetRetroBluePalette());
+				if (ImGui::MenuItem("Mariana palette"))
+					editor->SetPalette(TextEditor::GetMarianaPalette());
+				ImGui::EndMenu();
+			}
+			if (ImGui::BeginMenu("Misc"))
+			{
+				if (ImGui::MenuItem("BackLog View"))
+				{
+					std::cout << wi::backlog::getText() << std::endl;
+				}
+				ImGui::EndMenu();
+			}
+			ImGui::EndMenuBar();
+		}
+
+		ImGui::Text("%6d/%-6d %6d lines  | %s | %s | %s | %s", cpos.first + 1, cpos.second + 1, editor->GetLineCount(),
+			editor->IsOverwriteEnabled() ? "Ovr" : "Ins",
+			editor->CanUndo() ? "*" : " ",
+			editor->GetLanguageDefinition().mName.c_str(), "Temp.lua");
+		editor->Render("Lua Editor");
+		ImGui::Dummy(ImVec2(20, 20));
+		ImGui::End();
+	}
+	
 
 	
 	
